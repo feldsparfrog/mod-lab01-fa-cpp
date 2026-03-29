b37dcd5ad0994f468937773b2b262591e8b522bb
feldsparfrog <schepalovnickolay@gmail.com>
2026-03-29 09:44:56 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,14 +1,83 @@
-// Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <math.h>
 
-unsigned int faStr1(const char *str) {
-    return 0;
+unsigned int faStr1(const char* str)
+{
+	unsigned int wordCount = 0;
+	bool inWord = false;
+	bool isValidWord = true;
+	while (*str) 
+	{ 
+		if (*str != ' ' && !inWord)
+		{ 
+			if (*str < 48 || *str > 57) wordCount++;
+			else isValidWord = false;
+			inWord = true;
+		} 
+		else if (*str == ' ' && inWord)
+		{
+			inWord = false;
+			if (!isValidWord) isValidWord = true;
+		}
+		else if (*str >= 48 && *str <= 57 && inWord && isValidWord)
+		{
+			wordCount--;
+			isValidWord = false;
+		}
+		str++; 
+	} 
+	return wordCount;
 }
 
-unsigned int faStr2(const char *str) {
-    return 0;
+unsigned int faStr2(const char* str)
+{
+	unsigned int wordCount = 0;
+	bool inWord = false;
+	bool isValidWord = true;
+	while (*str)
+	{
+		if (*str != ' ' && !inWord)
+		{
+			if (*str >= 65 && *str <= 90) wordCount++;
+			else isValidWord = false;
+			inWord = true;
+		}
+		else if (*str == ' ' && inWord)
+		{
+			inWord = false;
+			if (!isValidWord) isValidWord = true;
+		}
+		else if ((*str < 97 || *str > 122) && inWord && isValidWord)
+		{
+			wordCount--;
+			isValidWord = false;
+		}
+		str++;
+	}
+	return wordCount;
 }
 
-unsigned int faStr3(const char *str) {
-    return 0;
+unsigned int faStr3(const char* str)
+{
+	unsigned int wordCount = 0;
+	unsigned int totalWordLength = 0;
+	bool inWord = false;
+	while (*str)
+	{
+		if (*str != ' ')
+		{
+			totalWordLength++;
+			if (!inWord)
+			{
+				wordCount++;
+				inWord = true;
+			}
+		}
+		else if (*str == ' ' && inWord)
+		{
+			inWord = false;
+		}
+		str++;
+	}
+	return round((float)totalWordLength / wordCount);
 }
