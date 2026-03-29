8b2f00e33ec908b5d91ec4d28f748f0976c14732
feldsparfrog <schepalovnickolay@gmail.com>
2026-03-29 09:46:21 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
-// Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <stdio.h>
 
-int main() {
-    return 0;
+int main()
+{
+	const char* testMessage = " GNEURSHK - 3500 is The Thing  of Fut43ure    until the fds23234 334h 4hh Est  s4";
+	unsigned int result1 = faStr1(testMessage);
+	unsigned int result2 = faStr2(testMessage);
+	unsigned int result3 = faStr3(testMessage);
+	printf("Words not containing numbers: %d\nWords starting on a capital letter and containing only latin letters: %d\nAverage word length: %d", result1, result2, result3);
+	return 0;
 }
